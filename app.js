06c948645ee0608cca10409a5e5c1407bf662d93
Kevin Peng <kevinpkf311@gmail.com>
2020-10-20 22:34:10 -0700

    Updated ___ function, added ____, fixed ___ bug, etc.
    :)))

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-//const express = require('express');
+const express = require('express');
 const port = 8000;
 
 const app = express();
@@ -8,10 +8,10 @@ app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
 });
 
-app.get('/introduction.js', (req, res) => {
+/*app.get('/introduction.js', (req, res) => {
     res.statusCode = 200;
     res.sendFile(__dirname + '/introduction.js');
-});
+});*/
 
 const server = app.listen(port, () => {
     console.log('Server started on port: ' + port);
