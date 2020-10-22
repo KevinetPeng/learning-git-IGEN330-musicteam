7e59987575185a959c08b182e2dc1340a7b5e20e
Kevin Peng <kevinpkf311@gmail.com>
2020-10-21 18:53:01 -0700

    woops again

--- introduction.js
+++ introduction.js
@@ -3,6 +3,9 @@ person = {
     age: 69,
 };
 
+person.name = "hehe";
+person.age = "30"
+
 function populateHTML() {
     document.getElementById("heading1").innerHTML = `Hi! my name is ${person.name}.`;
     document.getElementById("paragraph1").innerHTML = `I am ${person.age} years old.`;
