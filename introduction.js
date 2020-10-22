0720ebb6d9aaaa70914a8a8e9103eaad0a757fc1
Josiann Zhou <zhou.josi@gmail.com>
2020-10-21 21:56:11 -0400

    :))

--- introduction.js
+++ introduction.js
@@ -3,6 +3,9 @@ person = {
     age: 69,
 };
 
+person.name = "josie";
+person.age="21";
+
 function populateHTML() {
     document.getElementById("heading1").innerHTML = `Hi! my name is ${person.name}.`;
     document.getElementById("paragraph1").innerHTML = `I am ${person.age} years old.`;
